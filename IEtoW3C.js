edff679e45e3948eaae5b3e8bcf36580bbf813f7
Jason Johnston <github@lojjic.net>
2005-06-27 03:53:14 +0000

    * Wrap everything in an anonymous function, so all our functions are locally scoped instead of polluting the global scope. We no longer need a root IEtoW3C object. * Add Event interface and its constants * Implement EventException and its one defined constant code, and throw it at the correct time (would throw it at other times if i had some sort of guideline for what codes to use) * Sort of implement EventListener interface, in that if an object with a .handleEvent method is passed as the second argumetn of addEventListener, then that method is called with the object as its context.  Matches Gecko behavior and requested by S. Cramatte.

--- IEtoW3C.js
+++ IEtoW3C.js
@@ -11,55 +11,72 @@
 */
 
 
+// Initialize everything locally within an anonymous function so we don't pollute the global scope.
+(function() {
 
 /*=== DOM2 Events: ===*/
-if (!window.addEventListener && document.all /*(remove to enable partial (buggy) MacIE support:)*/ && window.attachEvent) {
+if(!window.Event) {
+	Event = {
+		CAPTURING_PHASE : 1,
+		AT_TARGET : 2,
+		BUBBLING_PHASE : 3
+	};
+}
 
-var IEtoW3C = {
+if(!window.EventException) {
+	function EventException(code, message) {
+		this.code = code;
+		this.message = message;
+	};
+	EventException.UNSPECIFIED_EVENT_TYPE_ERR = 0;
+	EventException.prototype = new Error;
+}
+
+if (!window.addEventListener && document.all /*(remove to enable partial (buggy) MacIE support:)*/ && window.attachEvent) {
 	// A list of the natively handled events, which can appear as "onevent" attributes:
 	// This is an incomplete list of native IE events, limited to the most common for
 	// performance reasons. If you need to use other native events, add them here.
-	nativeEvents : {mouseover:1,mouseout:1,mousemove:1,mousedown:1,mouseup:1,click:1,change:1,focus:1,blur:1,load:1,unload:1,keypress:1,keyup:1,keydown:1},
+	var nativeEvents = {mouseover:1,mouseout:1,mousemove:1,mousedown:1,mouseup:1,click:1,change:1,focus:1,blur:1,load:1,unload:1,keypress:1,keyup:1,keydown:1};
 
-	grabEventAttributes : function(elt) { //make on* attributes into DOM event listeners:
+	function grabEventAttributes(elt) { //make on* attributes into DOM event listeners:
 		if(!elt.attachEvent) return; //HACK: don't do this on Mac, since we have to preserve the onclick property later to listen for events.
 		if(elt.nodeType != 1) return; //elements only
-		for(var evtName in IEtoW3C.nativeEvents) {
+		for(var evtName in nativeEvents) {
 			var attr = (elt.getAttribute) ? elt.getAttribute("on"+evtName) : null;
 			if(attr) {
 				elt["IEtoW3C_on"+evtName] = elt["on"+evtName]; //use later to test if "return false" should preventDefault().
-				elt.addEventListener(evtName,IEtoW3C.execAttrEvent,false);
+				elt.addEventListener(evtName,execAttrEvent,false);
 				elt["on"+evtName] = null;
 			}
 		}
-	},
+	};
 
-	execAttrEvent : function(evt) {
+	function execAttrEvent(evt) {
 		var func = this["IEtoW3C_on"+evt.type].toString();
 		var funcBody = func.substring(func.indexOf("{")+1, func.lastIndexOf("}"));
 		funcBody = funcBody.replace(/([\W])this([\W])/,"$1event.currentTarget$2"); //fix "this" references
 		funcBody = "(function(event) {" + funcBody + "})(evt);";
 		var result = eval(funcBody);
 		if(result == false) evt.preventDefault(); //"return false;" prevents default action
-	},
+	};
 
-	stopPropagation : function() {
+	function stopPropagation() {
 		this.IEtoW3C_canceled = true;
-	},
+	};
 
-	preventDefault : function() {
+	function preventDefault() {
 		this.returnValue = false;
-	},
+	};
 
-	handleEvent : function(elt,evt) {
+	function handleEvent(elt,evt) {
 		//fixup event object with DOM properties and methods:
 		if(!evt) var evt = window.event;
 		if(!evt.currentTarget) evt.currentTarget = elt;
 		if(!evt.target) evt.target = evt.srcElement || elt;
 		if(evt.bubbles == null) evt.bubbles = true;
 		if(evt.cancelable == null) evt.cancelable = true;
-		evt.stopPropagation = IEtoW3C.stopPropagation;
-		evt.preventDefault = IEtoW3C.preventDefault;
+		evt.stopPropagation = stopPropagation;
+		evt.preventDefault = preventDefault;
 		if(!evt.relatedTarget) evt.relatedTarget = evt.fromElement || evt.toElement || null;
 		evt.pageX = evt.clientX + document.body.scrollLeft;
 		evt.pageY = evt.clientY + document.body.scrollTop;
@@ -69,15 +86,15 @@ var IEtoW3C = {
 		} else evt.charCode = 0;
 		evt.returnValue = true;
 		evt.IEtoW3C_canceled = false;
-		
+
 		//make sure the event doesn't bubble to ancestors so it doesn't get handled more than once:
 		evt.cancelBubble = true;
-		
+
 		//get all ancestors:
 		var ancestors = [];
 		var tmp = evt.target;
 		while(tmp) {
-			IEtoW3C.hookupDOMEventsOn(tmp);
+			hookupDOMEventsOn(tmp);
 			ancestors[ancestors.length] = tmp;
 			tmp = tmp.parentNode;
 		}
@@ -85,89 +102,92 @@ var IEtoW3C = {
 		tmp = ancestors[ancestors.length-1];
 		if(tmp === document.documentElement) tmp = ancestors[ancestors.length] = document;
 		if(tmp === document) ancestors[ancestors.length] = window;
-		
+
 		//fire each handler in correct order, passing the event object as sole parameter:
 		//capturing:
-		evt.eventPhase = 1;
+		evt.eventPhase = Event.CAPTURING_PHASE;
 		for(var i=ancestors.length-1; (tmp = ancestors[i]) && (!evt.cancelable || !evt.IEtoW3C_canceled); i--) {
-			if(i==0) evt.eventPhase = 2;
+			if(i==0) evt.eventPhase = Event.AT_TARGET;
 			evt.currentTarget = tmp;
 			var hdlrs = tmp.IEtoW3C_onevent[evt.type];
-			for(var j=0; hdlrs && j<hdlrs.capture.length; j++) {
-				tmp.IEtoW3C_handler = hdlrs.capture[j]; //make "this" refer to currentTarget
-				tmp.IEtoW3C_handler(evt);
-				tmp.IEtoW3C_handler = null;
-			}
+			for(var j=0,h; hdlrs && (h=hdlrs.capture[j]); j++) h.invoke(evt);
 		}
 		//bubbling:
 		for(var i=0; (tmp = ancestors[i]) && (!evt.cancelable || !evt.IEtoW3C_canceled); i++) {
 			evt.currentTarget = tmp;
 			var hdlrs = tmp.IEtoW3C_onevent[evt.type];
-			for(var j=0; hdlrs && j<hdlrs.bubble.length; j++) {
-				tmp.IEtoW3C_handler = hdlrs.bubble[j]; //make "this" refer to currentTarget
-				tmp.IEtoW3C_handler(evt);
-				tmp.IEtoW3C_handler = null;
-			}
+			for(var j=0, h; hdlrs && (h=hdlrs.bubble[j]); j++) h.invoke(evt);
 			if(i==0) {
 				if(!evt.bubbles) break;
-				evt.eventPhase = 3;
+				evt.eventPhase = Event.BUBBLING_PHASE;
 			}
 		}
-
 		//restore keyCode so form fields will get keystroke
 		if(evt.charCode > 0 && evt.keyCode == 0) evt.keyCode = evt.charCode;
-	},
+	};
 
-	addEventListener : function(evtType,handler,capture) {
-		var onevent = this.IEtoW3C_onevent[evtType];
+	function ListenerWrapper(elt, lst) {
+		this.listener = lst;
+		this.invoke = function(evt) {
+			if(typeof lst == "function") {
+				elt._lstnr = lst;
+				elt._lstnr(evt);
+				elt._lstnr = null;
+			}
+			else lst.handleEvent(evt);
+		};
+	};
+
+	function addEventListener(type,listener,useCapture) {
+		var onevent = this.IEtoW3C_onevent[type];
 		if(!onevent) {
-			onevent = this.IEtoW3C_onevent[evtType] = {capture:[],bubble:[]};
+			onevent = this.IEtoW3C_onevent[type] = {capture:[],bubble:[]};
 			//Set base listener to fire off custom event handling flow, but only for the "native"
 			//events, since there's no use in having IE listen for events that will never be fired.
 			//We cache a reference to the handler function so we can remove the listener during teardown
-			if(IEtoW3C.nativeEvents[evtType]) {
+			if(type in nativeEvents) {
 				var thisRef = this;
 				if(!this.IEtoW3C_listen) this.IEtoW3C_listen = {/*evtType : handler*/};
-				var lstn = this.IEtoW3C_listen[evtType] = function() { IEtoW3C.handleEvent(thisRef) };
-				if(this.attachEvent) this.attachEvent("on"+evtType, lstn);
+				var lstn = this.IEtoW3C_listen[type] = function() { handleEvent(thisRef) };
+				if(this.attachEvent) this.attachEvent("on"+type, lstn);
 				else this["on"+evtType] = lstn; //IE Mac
 			}
 		}
-		this.removeEventListener(evtType,handler,capture); //avoid duplicates
-		var handlers = (capture) ? onevent.capture : onevent.bubble;
-		handlers[handlers.length] = handler;
-	},
+		var wrapper = new ListenerWrapper(this, listener);
+		this.removeEventListener(type,listener,useCapture); //avoid duplicates
+		var list = (useCapture) ? onevent.capture : onevent.bubble;
+		list[list.length] = wrapper;
+	};
 
-	removeEventListener : function(evtType,handler,capture) {
-		var onevent = this.IEtoW3C_onevent[evtType];
+	function removeEventListener(type,listener,useCapture) {
+		var onevent = this.IEtoW3C_onevent[type];
 		if(!onevent) return;
-		var handlers = (capture) ? onevent.capture : onevent.bubble;
-		for(var i=0; i<handlers.length; i++) { //remove any instances of handler from list:
-			if(handlers[i] === handler) {
-				for(var j=i; j<handlers.length-1; j++) handlers[j] = handlers[j+1];
-				handlers.length--;
+		var list = (useCapture) ? onevent.capture : onevent.bubble;
+		for(var i=0; i<list.length; i++) { //remove any instances of handler from list:
+			if(list[i].listener === listener) {
+				for(var j=i; j<list.length-1; j++) list[j] = list[j+1];
+				list.length--;
 			}
 		}
-	},
+	};
 
-	dispatchEvent : function(evt) {
-		if(!evt.type) return;
+	function dispatchEvent(evt) {
+		if(!evt.type) throw new EventException(EventException.UNSPECIFIED_EVENT_TYPE_ERR);
 		evt.target = evt.currentTarget = this;
-		IEtoW3C.handleEvent(this,evt);
-	},
+		handleEvent(this,evt);
+	};
 
-	hookupDOMEventsOn : function(elt) {
+	function hookupDOMEventsOn(elt) {
 		if(elt.IEtoW3C_onevent) return;
 		elt.IEtoW3C_onevent = {};
+		elt.addEventListener = addEventListener;
+		elt.removeEventListener = removeEventListener;
+		elt.dispatchEvent = dispatchEvent;
+		grabEventAttributes(elt);
+	};
 
-		elt.addEventListener = IEtoW3C.addEventListener;
-		elt.removeEventListener = IEtoW3C.removeEventListener;
-		elt.dispatchEvent = IEtoW3C.dispatchEvent;
-		IEtoW3C.grabEventAttributes(elt);
-	},
-	
-	unhookDOMEventsFrom : function(elt) {
-		// detach all listeners attached in IEtoW3C.addEventListener above:
+	function unhookDOMEventsFrom(elt) {
+		// detach all listeners attached in addEventListener above:
 		if(elt.IEtoW3C_listen) {
 			for(var i in elt.IEtoW3C_listen) {
 				if(elt.detachEvent) elt.detachEvent("on"+i, elt.IEtoW3C_listen[i]);
@@ -175,67 +195,67 @@ var IEtoW3C = {
 			}
 		}
 		// remove cached "onevent" attributes:
-		for(var evtName in IEtoW3C.nativeEvents) {
+		for(var evtName in nativeEvents) {
 			elt["IEtoW3C_on"+evtName] = null;
 		}
 		// destroy all other added properties/methods:
 		elt.IEtoW3C_listen = elt.IEtoW3C_onevent = elt.addEventListener = elt.removeEventListener = elt.dispatchEvent = null;
-	}
-};
-
-document.createEvent = function(evtFam) {
-	var evt = {}; //new Event object
-	if(evtFam=="UIEvents") {
-		evt.initUIEvent = function(/*t,b,c,v,d*/) {
-			var initArgs = ["type","bubbles","cancelable","view","detail"];
-			for(var i=0,x; (x=initArgs[i]); i++) this[x]=arguments[i];
-		};
-	}
-	else if(evtFam=="MouseEvents") {
-		evt.initMouseEvent = function(/*t,b,c,v,d,sx,sy,cx,cy,ck,ak,sk,mk,b,r*/) {
-			var initArgs = ["type","bubbles","cancelable","view","detail","screenX","screenY","clientX","clientY","ctrlKey","altKey","shiftKey","metaKey","button","relatedTarget"];
-			for(var i=0,x; (x=initArgs[i]); i++) this[x]=arguments[i];
-		};
-	}
-	else if(evtFam=="MutationEvents") {
-		evt.initMutationEvent = function(/*t,b,c,r,p,n,an,ac*/) {
-			var initArgs = ["type","bubbles","cancelable","relatedNode","prevValue","newValue","attrName","attrChange"];
-			for(var i=0,x; (x=initArgs[i]); i++) this[x]=arguments[i];
-		};
-	}
-	else evt.initEvent = function(type,bub,can) {
-		this.type = type; this.bubbles = bub; this.cancelable = can;
 	};
-	return evt;
-};
 
-IEtoW3C.hookupDOMEventsOn(window);
-IEtoW3C.hookupDOMEventsOn(document);
-window.addEventListener("load",function() {
-	var all = document.all;
-	for(var i=0; i<all.length; i++) IEtoW3C.hookupDOMEventsOn(all[i]);
-	
-	//set dummy listeners for mouse events that natively fire on document but
-	//not on window, so they will bubble up in our custom loop:
-	var dummies = ["click","mouseover","mouseout","mousemove"];
-	for(var i=0; i<dummies.length; i++) document.addEventListener(dummies[i],function(){},false);
-},false);
+	document.createEvent = function(evtFam) {
+		var evt = {}; //new Event object
+		if(evtFam=="UIEvents") {
+			evt.initUIEvent = function(/*t,b,c,v,d*/) {
+				var initArgs = ["type","bubbles","cancelable","view","detail"];
+				for(var i=0,x; (x=initArgs[i]); i++) this[x]=arguments[i];
+			};
+		}
+		else if(evtFam=="MouseEvents") {
+			evt.initMouseEvent = function(/*t,b,c,v,d,sx,sy,cx,cy,ck,ak,sk,mk,b,r*/) {
+				var initArgs = ["type","bubbles","cancelable","view","detail","screenX","screenY","clientX","clientY","ctrlKey","altKey","shiftKey","metaKey","button","relatedTarget"];
+				for(var i=0,x; (x=initArgs[i]); i++) this[x]=arguments[i];
+			};
+		}
+		else if(evtFam=="MutationEvents") {
+			evt.initMutationEvent = function(/*t,b,c,r,p,n,an,ac*/) {
+				var initArgs = ["type","bubbles","cancelable","relatedNode","prevValue","newValue","attrName","attrChange"];
+				for(var i=0,x; (x=initArgs[i]); i++) this[x]=arguments[i];
+			};
+		}
+		else evt.initEvent = function(type,bub,can) {
+			this.type = type; this.bubbles = bub; this.cancelable = can;
+		};
+		return evt;
+	};
 
-window.addEventListener("unload",function() { //clean up:
-	IEtoW3C.unhookDOMEventsFrom(window);
-	IEtoW3C.unhookDOMEventsFrom(document);
-	var all = document.all;
-	for(var i=0; i<all.length; i++) IEtoW3C.unhookDOMEventsFrom(all[i]);
-	document.createEvent = document.IEtoW3C_createElement = document.createElement = IEtoW3C = null;
-},false);
+	hookupDOMEventsOn(window);
+	hookupDOMEventsOn(document);
 
-//make it work for newly created elements:
-document.IEtoW3C_createElement = document.createElement;
-document.createElement = function(tagName) {
-	var newElt = document.IEtoW3C_createElement(tagName);
-	IEtoW3C.hookupDOMEventsOn(newElt);
-	return newElt;
-};
+	window.addEventListener("load",function() {
+		var all = document.all;
+		for(var i=0; i<all.length; i++) hookupDOMEventsOn(all[i]);
+		
+		//set dummy listeners for mouse events that natively fire on document but
+		//not on window, so they will bubble up in our custom loop:
+		var dummies = ["click","mouseover","mouseout","mousemove"];
+		for(var i=0; i<dummies.length; i++) document.addEventListener(dummies[i],function(){},false);
+	},false);
+
+	window.addEventListener("unload",function() { //clean up:
+		unhookDOMEventsFrom(window);
+		unhookDOMEventsFrom(document);
+		var all = document.all;
+		for(var i=0; i<all.length; i++) unhookDOMEventsFrom(all[i]);
+		document.createEvent = document.IEtoW3C_createElement = document.createElement = IEtoW3C = null;
+	},false);
+
+	//make it work for newly created elements:
+	document.IEtoW3C_createElement = document.createElement;
+	document.createElement = function(tagName) {
+		var newElt = document.IEtoW3C_createElement(tagName);
+		hookupDOMEventsOn(newElt);
+		return newElt;
+	};
 }
 
 /*=== DOM2 Core: ===*/
@@ -259,7 +279,7 @@ if(!document.implementation.createDocument && window.ActiveXObject) {
 			throw "No MSXML found on system";
 		};
 		var newDoc = new ActiveXObject(getProgID());
-		IEtoW3C.hookupDOMEventsOn(newDoc);
+		hookupDOMEventsOn(newDoc);
 		newDoc.onreadystatechange = function() {
 			if(this.readyState == 4) {
 				if(this.onload) this.onload();
@@ -329,3 +349,5 @@ if(!window.getComputedStyle) {
 		};
 	};
 }
+
+})();
